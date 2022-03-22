d03f3d9b9df4320bc8673f767a3a36fbdea22c3f
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2022-03-22 18:55:37 +0530

    gcd in cpp

---
+++ gcd.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if(b==0 && a == 0)
+        return 0;
+        
+    if(a == 0)
+    {
+        return b;
+    }
+    else if(b == 0)
+    {
+        return a;
+    }
+    
+    if(b == 0)
+    {
+        return a;
+    }
+    else{
+        return gcd(a, b - a);
+    }
+    
+    // if(a == b)
+    //     return a;
+    // else if(a < b)
+    //     return gcd(a, b - a);
+    // else
+    //     return gcd(a - b, b);
+    
+}
+
+int main() {
+    // cout<<__gcd(24, 12);
+    int a = 24, b = 12;
+    
+    cout<<gcd(min(a, b), max(a,b));
+    return 0;
+}
