f8f7c88b4a746bb5eaebe3b40bc4c9b4cad45911
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2022-03-23 23:26:57 +0530

    Create sieve_of_Eratosthenes.cpp

---
+++ sieve_of_Eratosthenes.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n = 50;
+int primes[50];
+void generate_seive(){
+    for (int i=0;i<50;i++)primes[i]=true;
+    primes[0]=primes[1]=false;
+    for(int i=2; i*i<=n; i++)
+    {
+        if(primes[i]==true)
+        {
+            for(int j=i*i;j<50;j+=i)
+            {
+                primes[j]=false;
+            }
+        }
+    }
+}
+int main()
+{
+    generate_seive();
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int num;
+        cin>>num;
+        if(primes[num] == true)
+            cout<<"It is prime number";
+        else
+            cout<<"It is not prime number";
+    }
+}
