b722294ad665b65e442132528fea31e6b04689fa
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2022-03-24 17:39:41 +0530

    Update sieve_of_Eratosthenes.cpp

--- sieve_of_Eratosthenes.cpp
+++ sieve_of_Eratosthenes.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int n = 50;
 int primes[50];
 void generate_seive(){
-    for (int i=0;i<50;i++)primes[i]=true;
+    for (int i=0;i<n;i++)primes[i]=true;
     primes[0]=primes[1]=false;
     for(int i=2; i*i<=n; i++)
     {
         if(primes[i]==true)
         {
-            for(int j=i*i;j<50;j+=i)
+            for(int j=i*i;j<n;j+=i)
             {
                 primes[j]=false;
             }
