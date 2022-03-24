c8190cf6c8f1855d5abdcae6cbbbcf17b4a70847
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2022-03-24 14:34:09 +0530

    using square root

---
+++ prime_sqrt.py
@@ -0,0 +1,11 @@
+import math
+n=int(input("Enter a number "))
+m=int(math.sqrt(n))
+k=0
+for i in range(2,m//2+1):
+    if m%i==0:
+        k=k+1
+if k==0:
+    print(m,", which square root of ",n," , is Prime Number.")
+else:
+    print(m,", which square root of ",n," , is NOt Prime Number")
