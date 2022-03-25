86a527012706f5ced8bac53d7d61d22a9ea87fa8
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2022-03-25 12:12:46 +0530

    Update ascii.cpp

--- ascii.cpp
+++ ascii.cpp
@@ -12,10 +12,26 @@ int returnVal (char x)                  // to convert character to integer
 int main()
 {
     char p = 'j';
-    cout<<"The integer value of "<<p<<" is: "<<returnVal(p)<<"\n"; 
-    int q = 'a';
-    cout << q<<"\n";                         // will print 97
-    int x = 'a';
-    cout << x - 96;                          // will print 1
+    cout<<"The index value of "<<p<<" in aplhabetical index is: "<<returnVal(p)<<"\n";
+    
+    cout<<"Enter a alphabet index: \n";
+    int i;
+    cin>>i;
+    char alpha = 96 + i;
+    cout<<"the character in the alphabetical series index is: "<<alpha<<"\n";
+    
+    cout<<"\n"; 
+    
+    cout<<"Enter a character to get the ASCII value of it: \n";
+    char q;
+    cin>>q;
+    cout<<"The ASCII value of is: "<<int(q)<<"\n";          
+    
+    cout<<"\n";
+    
+    cout<<"Enter the character to get the alphabet index: \n";
+    char x;
+    cin>>x;
+    cout<<"The alphabet series index of "<<x<<" is: "<<int(x - 96);                          
     return 0;
 }
