dfb661e051a123eebc0ebb7134b91f00f4c8f692
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2022-03-26 18:28:43 +0530

    Create set.cpp

---
+++ DataStructures/set.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// set arrange them in ascending order(sorted order), no duplicates 
+	set<int>se;
+	se.insert(1); //{1}
+	se.insert(2); //{1, 2}
+	se.insert(4); //{1,2,4}
+	se.emplace(3); //{1,2,3,4}
+	auto it=se.find(3);
+	cout<<*(it);
+	cout<<"\n";
+	for(auto it: se)
+	{
+		cout<<it<<" ";
+	}
+}
